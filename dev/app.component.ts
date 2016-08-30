9c4ef546173852acae79595bb0e7c29dced5ad85
Viktar Yafimchyk <viktar.yafimchyk@carsales.com.au>
2016-08-30 10:08:20 +1000

    Completed tutorial at https://youtu.be/wZ_5des_6_c

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
+import {ShoppingListComponent} from './shopping-list-component';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        This is the app component
+        <shoppping-list></shoppping-list>
     `,
+    directives: [ShoppingListComponent]
 })
 export class AppComponent {
 
